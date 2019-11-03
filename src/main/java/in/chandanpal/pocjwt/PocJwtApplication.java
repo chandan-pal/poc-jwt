c869af59812c838d36ac0155c69ea8d92c220f58
chandan-pal <chandanpal290190@gmail.com>
2019-11-03 13:57:08 +0530

    spring initialize package

---
+++ src/main/java/in/chandanpal/pocjwt/PocJwtApplication.java
@@ -0,0 +1,13 @@
+package in.chandanpal.pocjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJwtApplication.class, args);
+	}
+
+}
